a538d72a26152eb2535f7341d2393fe7e986ae52
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2024-06-12 10:56:06 +0700

    ci-cd

---
+++ modules/storage_account/storage_account/storage_account.tf
@@ -0,0 +1,32 @@
+resource "azurerm_storage_account" "storage" {
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  location                 = var.location
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_storage_container" "create_container" {
+  for_each = {
+    source = var.source_folder_name,
+    destination = var.destination_folder_name
+  }
+  name = each.key
+  storage_account_name = azurerm_storage_account.storage.name
+  container_access_type = var.container_access_type
+}
+
+resource "azurerm_storage_blob" "create_test_file" {
+  name                   = "test.text"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.create_container["source"].name
+  type                   = "Block"
+  source_content = "Hello world from Phuc An!"
+}
+
+output "storage_account_key" {
+  value = azurerm_storage_account.storage.primary_access_key
+}
---
+++ variables.tfvars
@@ -0,0 +1,9 @@
+resource_group_name = "phucan-rg"
+storage_account_name = "phucanvn"
+location = "southeastasia"
+
+tags = {
+  Enviroment : "development"
+}
+source_folder_name = "source"
+destination_folder_name = "destination"
